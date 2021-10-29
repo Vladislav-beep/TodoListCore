8782faba641084cbd75abb69d5b801a01fafa74c
VLADISLAV <SIZONOV.95@MAIL.RU>
2021-10-29 16:33:34 +0300

    final

--- TodoListCore/TableViewController.swift
+++ TodoListCore/TableViewController.swift
@@ -12,8 +12,41 @@ class TableViewController: UITableViewController {
     
     var tasks: [Task] = []
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let context = getContext()
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        do {
+            tasks = try context.fetch(fetchRequest)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let context = getContext()
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        if let objects = try? context.fetch(fetchRequest) {
+            for object in objects {
+                context.delete(object)
+            }
+        }
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func getContext() -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
     }
     
     @IBAction func saveTask(_ sender: UIBarButtonItem) {
@@ -35,8 +68,7 @@ class TableViewController: UITableViewController {
     }
     
     private func saveTask(withTitle title: String) {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
+        let context = getContext()
         
         guard let entity = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
         let taskObject = Task(entity: entity, insertInto: context)
@@ -44,6 +76,7 @@ class TableViewController: UITableViewController {
         
         do {
             try context.save()
+            tasks.append(taskObject )
         } catch let error as NSError {
             print(error.localizedDescription)
         }
